8838bbd5cc3ed9b93095fd979374d03ecabcaf5b
osman <butt.osman@gmail.com>
2021-09-30 22:56:28 +0200

    Implemented ceasarEncoder

--- src/CodeBreaker.java
+++ src/CodeBreaker.java
@@ -7,6 +7,10 @@ public class CodeBreaker {
         String decodedString = numberCypherDecoder(encodedString);
         System.out.println(encodedString);
         System.out.println(decodedString);
+        String encodedCeasar = ceasarEncoder("hello world");
+        System.out.println(encodedCeasar);
+//        System.out.println((int) 'h'+3);
+//        System.out.println((char) (107));
     }
 
     public static String numberCypherEncoder(String decodedString){
@@ -14,7 +18,7 @@ public class CodeBreaker {
         String encodedString = "";
         for (int i = 0; i<decodedStringToLower.length()-1;i++){
             char decodedChar = decodedStringToLower.charAt(i);
-            int numberInAlphabet = decodedChar - 'a' + 1;
+            int numberInAlphabet = decodedChar - 'a' + 1; //making 'a'=1
             encodedString += numberInAlphabet;
             encodedString += ";";
         }
@@ -25,7 +29,7 @@ public class CodeBreaker {
     }
 
     public static String numberCypherDecoder(String encodedString){
-        // Letter a to z runs has code points ranging from 97 to 122 in java
+        // Letter a to z has code points ranging from 97 to 122 in java
         int codePointStart = (int) 'a' - 1 ;
         String decodedString = "";
         String[] encodedStringToArray = encodedString.split(";");
@@ -39,4 +43,26 @@ public class CodeBreaker {
         }
         return decodedString;
     }
+
+    public static String ceasarEncoder(String decodedString){
+        int amountToShiftAlphabet = 3;
+        String decodedStringToLower = decodedString.toLowerCase();
+        String encodedString = "";
+        //Convert string to char array
+        char[] charArray = new char[decodedStringToLower.length()];
+        for (int i = 0; i<decodedStringToLower.length(); i++){
+            charArray[i] = decodedStringToLower.charAt(i);
+        }
+        for (int i = 0; i<decodedStringToLower.length(); i++){
+            if (charArray[i]==' '){
+                encodedString += " ";
+            }else{
+                int charToIntShifted =  charArray[i] + amountToShiftAlphabet;
+                char shiftedIntToChar = (char) (charToIntShifted);
+                encodedString += shiftedIntToChar;
+            }
+        }
+        return encodedString;
+    }
+
 }
