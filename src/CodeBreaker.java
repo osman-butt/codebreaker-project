e93fadd5f0a0a4a98658c04b056530471681f25a
osman <butt.osman@gmail.com>
2021-09-30 22:37:02 +0200

    Implemented numberCypherDecoder

--- src/CodeBreaker.java
+++ src/CodeBreaker.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
+
 public class CodeBreaker {
     public static void main(String[] args) {
-        String decodedString = "Osman Butt";
-        String encodedString = numberCypherEncoder(decodedString);
+        String s = "Osman Butt";
+        String encodedString = numberCypherEncoder(s);
+        String decodedString = numberCypherDecoder(encodedString);
         System.out.println(encodedString);
+        System.out.println(decodedString);
     }
 
     public static String numberCypherEncoder(String decodedString){
@@ -10,17 +14,29 @@ public class CodeBreaker {
         String encodedString = "";
         for (int i = 0; i<decodedStringToLower.length()-1;i++){
             char decodedChar = decodedStringToLower.charAt(i);
-            if (decodedChar == ' '){
-                encodedString += " ;";
-            }else{
-                int numberInAlphabet = decodedChar - 'a' + 1;
-                encodedString += numberInAlphabet;
-                encodedString += ";";
-            }
+            int numberInAlphabet = decodedChar - 'a' + 1;
+            encodedString += numberInAlphabet;
+            encodedString += ";";
         }
         char lastChar = decodedStringToLower.charAt(decodedStringToLower.length()-1);
         int numberInAlphabet = lastChar - 'a' + 1;
         encodedString += numberInAlphabet;
         return encodedString;
+    }
+
+    public static String numberCypherDecoder(String encodedString){
+        // Letter a to z runs has code points ranging from 97 to 122 in java
+        int codePointStart = (int) 'a' - 1 ;
+        String decodedString = "";
+        String[] encodedStringToArray = encodedString.split(";");
+        int[] encodedStringToIntArray = new int[encodedStringToArray.length];
+        for (int i = 0; i < encodedStringToArray.length; i++) {
+            encodedStringToIntArray[i] = Integer.valueOf(encodedStringToArray[i]);
+        }
+        for (int i = 0; i<encodedStringToIntArray.length; i++){
+            char decodedChar = (char) (encodedStringToIntArray[i] + codePointStart);
+            decodedString += decodedChar;
+        }
+        return decodedString;
     }
 }
